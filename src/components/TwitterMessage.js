da72d41be6f2b3f115f16bc0605d6de5aa2ffa53
Raza <rtshareef@gmail.com>
2019-10-31 11:09:18 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,17 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    // initial value of message is blank
+    this.state = {
+      message: '',
+    };
   }
 
+
+  // on the even of the data changing the state gets changed based on the value of the input and the maxchars props gets smaller as the message gets longer
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" onChange={event => this.setState({message: event.target.value})} value={this.state.message}/>
+        {this.props.maxChars-this.state.message.length}
       </div>
     );
   }
 }
+
 
 export default TwitterMessage;
